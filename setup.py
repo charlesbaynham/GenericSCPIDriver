2afa4f4b9c3f21ebdedf1986d54c8aee33d341f0
Charles Baynham <charles.baynham@npl.co.uk>
2020-06-19 18:13:46 +0100

    Add deps

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description="A generic template for creating python object-based drivers for SCPI hardware devices which communicate via VISA. Compatible with ARTIQ. ",
     long_description=read("README.rst"),
     packages=find_packages(exclude=("tests",)),
-    install_requires=[],
+    install_requires=["pyvisa"],
     extras_require={"dev": ["pre-commit", "tox", "sphinx", "sphinx_rtd_theme"]},
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
