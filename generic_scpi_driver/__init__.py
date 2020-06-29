e71bec292515f40785ba43951bb3857394caca34
Fara <ssr1@npl.co.uk>
2020-06-29 11:39:46 +0000

    Export decorators

--- generic_scpi_driver/__init__.py
+++ generic_scpi_driver/__init__.py
@@ -1,10 +1,10 @@
 """generic_scpi_driver - A generic template for creating python object-based drivers for SCPI hardware devices which communicate via VISA. Compatible with ARTIQ. """
 
 from ._version import get_versions
-from .driver import GenericDriver
+from .driver import GenericDriver, with_handler, with_lock
 
 __version__ = get_versions()["version"]
 del get_versions
 
 __author__ = "Charles Baynham <charles.baynham@npl.co.uk>"
-__all__ = ["GenericDriver"]
+__all__ = ["GenericDriver", "with_handler", "with_lock"]
