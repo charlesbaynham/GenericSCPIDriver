d3a9455fc9af21ce06771c758301057fb2867ac6
Charles Baynham <c.baynham@imperial.ac.uk>
2024-09-19 15:35:12 +0100

    Version bump

--- generic_scpi_driver/__init__.py
+++ generic_scpi_driver/__init__.py
@@ -1,4 +1,5 @@
 """generic_scpi_driver - A generic template for creating python object-based drivers for SCPI hardware devices which communicate via VISA. Compatible with ARTIQ. """
+
 from .driver import GenericDriver
 from .driver import with_handler
 from .driver import with_lock
@@ -7,4 +8,4 @@ from .generic_aqctl import get_controller_func
 
 __author__ = "Charles Baynham <charles.baynham@npl.co.uk>"
 __all__ = ["GenericDriver", "with_handler", "with_lock", "get_controller_func"]
-__version__ = "1.5.3"
+__version__ = "1.6"
