53b108a49a5470522e2cb753cb99000c5adf9823
Charles Baynham <c.baynham@imperial.ac.uk>
2025-10-21 21:15:45 +0100

    Update sipyco python requirement and bump version to 1.6.4

--- generic_scpi_driver/__init__.py
+++ generic_scpi_driver/__init__.py
@@ -7,7 +7,7 @@ from .driver import with_lock
 __author__ = "Charles Baynham <charles.baynham@npl.co.uk>"
 
 __all__ = ["GenericDriver", "with_handler", "with_lock"]
-__version__ = "1.6.3"
+__version__ = "1.6.4"
 
 
 try:
